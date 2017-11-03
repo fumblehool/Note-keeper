62a0f79162a90afd169cdff006f9080af7db7f08
Damanpreet <daman.4880@gmail.com>
2017-11-03 15:47:45 +0530

    completed header styling

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,21 +5,37 @@ import React, { Component } from 'react';
 class Header extends Component{
   render () {
     return(
-      <div className="header b-red">
-        <div className="header-item-search b-blue">
-          <div>
-            <h2>search</h2>
+      <div className="header">
+        <div className="header-search">
+          
+          <h2>All Stars</h2>
+          
+          <div className="header-settings">
+            <i className="fa fa-cog m-right"></i>
           </div>
-          <div>
-            <form>
-              <input type="text"/>
-            </form>
+          
+
+          <div className="header-form">
+            <label>
+              <span className="input-group-addon">
+                <i className="fa fa-search"></i>
+              </span>  
+              <input type="text" placeholder="search"/>
+              
+            </label>
           </div> 
+          
+          
         </div>
-        Header
-        <div className="header-item-user b-blue">
+
+        <div className="header-status">
+            Status
+          </div>  
+
+
+        <div className="header-user">
           <img src="https://avatars0.githubusercontent.com/u/8335453?v=4" className="user-avatar"/>
-          <span>Damanpreet Singh</span>
+          <span className="username m-right">Damanpreet Singh</span>
           <i className="fa fa-chevron-down"></i>
         </div>
       </div>
