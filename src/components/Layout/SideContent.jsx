899376f50a254ef4952a11ffc0ce0f986778f602
Damanpreet <daman.4880@gmail.com>
2017-11-03 17:02:14 +0530

    completed sideContent styling

--- src/components/Layout/SideContent.jsx
+++ src/components/Layout/SideContent.jsx
@@ -3,17 +3,36 @@ import React, { Component } from 'react';
 class SideContent extends Component{
   render() {
     return(
-      <ul id="L" className="content-sidebar-ul">
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-          <li className="content-sidebar-li"> Item 1 </li>
-        </ul>  
+      <ul className="content-sidebar-repos">
+        <li className="content-sidebar-li">
+          <h3 className="content-sidebar-li-h3">
+            GoogleChrome/samples
+            </h3> 
+          <div className="content-sidebar-li-description">
+            A repo containing samples tied to new functionality in each release of Google Chrome.
+          </div>
+          <ul className="content-sidebar-li-tags">
+            <li>test</li>
+            <li>test2</li>
+          </ul>
+          <div className="content-sidebar-li-repo-stats">
+            <div className="stars">
+              <i className="fa fa-star m-right"></i>
+              3088
+            </div>
+            <div className="forks">
+              <i className="fa fa-code-fork m-right"></i>
+              1206
+            </div>
+            <div className="link">
+              <a href="#">
+                View on GitHub
+              </a>
+            </div>
+          </div>
+ 
+        </li>
+      </ul>  
     );
   }
 }
